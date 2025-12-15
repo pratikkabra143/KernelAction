53f55a838df4da22ae948468e6fdd05ef5ad21c9
RainyXeon <rainyxeon@gmail.com>
2025-12-15 20:21:25 +0700

    fix: version string in hypermoon

--- scripts/hypermoon-exp-version.sh
+++ scripts/hypermoon-exp-version.sh
@@ -1,9 +1,6 @@
-echo "Add experimental version string"
-version_string="${cat 'localversion-hypermoon'}-CI"
-echo $version_string > localversion-00-experimental
 echo "Add commit version string"
-echo "-$(git rev-parse --short=7 HEAD)" > localversion-01-experimental
+echo "-$(git rev-parse --short=7 HEAD)" > localversion-hypermoon-1
 echo "Add run number string"
-echo "-$RUN_NUMBER" > localversion-02-experimental
+echo "-$RUN_NUMBER" > localversion-hypermoon-2
 make O=out kernelrelease
 echo "Done!"
