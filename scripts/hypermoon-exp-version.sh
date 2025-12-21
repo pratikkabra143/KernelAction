c17f0af3c6398481cef720457561a5a961af7f20
RainyXeon <rainyxeon@gmail.com>
2025-12-21 11:05:34 +0700

    remove: unused config and update hypermoon

--- scripts/hypermoon-exp-version.sh
+++ scripts/hypermoon-exp-version.sh
@@ -1,6 +1,10 @@
+echo "Remove Official Local Version String"
+rm "localversion-hypermoon"
+echo "Add experimental version string"
+echo "-HyperMoon-CI" > localversion-00-experimental
 echo "Add commit version string"
-echo "-$(git rev-parse --short=7 HEAD)" > localversion-hypermoon-1
+echo "-$(git rev-parse --short=7 HEAD)" > localversion-01-experimental
 echo "Add run number string"
-echo "-$RUN_NUMBER" > localversion-hypermoon-2
+echo "-$RUN_NUMBER" > localversion-02-experimental
 make O=out kernelrelease
 echo "Done!"
