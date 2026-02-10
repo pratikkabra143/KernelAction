a7af7a8411d1b3020e0d20ab412e908e14f53190
Pratik Kabra <98254405+pratikkabra143@users.noreply.github.com>
2026-02-10 08:32:19 +0530

    Update experimental version string in script

--- scripts/hypermoon-exp-version.sh
+++ scripts/hypermoon-exp-version.sh
@@ -1,7 +1,7 @@
 echo "Remove Official Local Version String"
 rm "localversion-hypermoon"
 echo "Add experimental version string"
-echo "-HyperMoon-CI" > localversion-00-experimental
+echo "-pratikkabra143" > localversion-00-experimental
 echo "Add commit version string"
 echo "-$(git rev-parse --short=7 HEAD)" > localversion-01-experimental
 echo "Add run number string"
